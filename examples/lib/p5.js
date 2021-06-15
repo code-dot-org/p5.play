d92d68f9269333f80e88b6b49284549c237adef7
Erin Peach <erin@code.org>
2021-06-15 15:21:38 -0700

    Use 0 as lowest value

--- examples/lib/p5.js
+++ examples/lib/p5.js
@@ -19935,12 +19935,12 @@ p5.prototype.tint = function () {
  * @param {Number} alpha  opacity of the sprite
  */
 p5.prototype.alphaTint = function () {
-  // alpha should be between 0.01 and 1
+  // Enforce that alpha is between 0 and 1
   var alpha = arguments[0];
   if (typeof alpha === "string") {
     alpha = Number(alpha);
   }
-  alpha = Math.max(alpha, 0.01);
+  alpha = Math.max(alpha, 0);
   alpha = Math.min(alpha, 1);
   this._renderer._alpha = alpha;
 };
